b5256c994751384532e6eb8db619274864c9b06d
jobsamalukyuhimuna-a11y <jobs.amalukyuhimuna@gmail.com>
2026-07-12 17:01:59 +0300

    Update universes section

--- app/components/sections/FeaturedUniverses.tsx
+++ app/components/sections/FeaturedUniverses.tsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 
 const universes = [
   {
-    title: "Ithan Ithan",
-    subtitle: "The King Without a Throne",
+    title: "Isan Ithan",
+    subtitle: "The King Without A Kingdom Novel",
     href: "/universes",
   },
   {
