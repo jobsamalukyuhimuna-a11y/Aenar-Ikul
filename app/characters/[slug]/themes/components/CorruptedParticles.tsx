31a1fd385b0d4ef4daf9d1b1367f352dfcf570d3
jobsamalukyuhimuna-a11y <jobs.amalukyuhimuna@gmail.com>
2026-07-25 23:50:25 +0300

    Optimize Corrupted theme performance

--- app/characters/[slug]/themes/components/CorruptedParticles.tsx
+++ app/characters/[slug]/themes/components/CorruptedParticles.tsx
@@ -3,62 +3,67 @@
 export default function CorruptedParticles() {
   return (
     <>
-      {Array.from({ length: 220 }).map((_, i) => (
+      {/* Main Corrupted Particles */}
+      {Array.from({ length: 120 }).map((_, i) => (
         <span
           key={`particle-${i}`}
           className="corrupted-particle"
           style={{
             left: `${(i * 17) % 100}%`,
-            animationDelay: `${i * 0.06}s`,
-            animationDuration: `${7 + (i % 6)}s`,
+            animationDelay: `${i * 0.08}s`,
+            animationDuration: `${9 + (i % 5)}s`,
           }}
         />
       ))}
 
-      {Array.from({ length: 120 }).map((_, i) => (
+      {/* Purple Sparks */}
+      {Array.from({ length: 60 }).map((_, i) => (
         <span
           key={`spark-${i}`}
           className="corrupted-spark"
           style={{
             left: `${(i * 21) % 100}%`,
             top: `${(i * 13) % 100}%`,
-            animationDelay: `${i * 0.09}s`,
+            animationDelay: `${i * 0.12}s`,
           }}
         />
       ))}
 
-      {Array.from({ length: 80 }).map((_, i) => (
+      {/* Floating Ash */}
+      {Array.from({ length: 40 }).map((_, i) => (
         <span
           key={`ash-${i}`}
           className="corrupted-ash"
           style={{
             left: `${(i * 31) % 100}%`,
-            animationDelay: `${i * 0.18}s`,
-            animationDuration: `${12 + (i % 5)}s`,
+            animationDelay: `${i * 0.22}s`,
+            animationDuration: `${14 + (i % 4)}s`,
           }}
         />
       ))}
 
-      {Array.from({ length: 35 }).map((_, i) => (
+      {/* Smoke */}
+      {Array.from({ length: 18 }).map((_, i) => (
         <span
           key={`smoke-${i}`}
           className="corrupted-smoke"
           style={{
             left: `${(i * 29) % 100}%`,
-            animationDelay: `${i * 0.6}s`,
-            animationDuration: `${16 + (i % 6)}s`,
+            animationDelay: `${i * 0.8}s`,
+            animationDuration: `${18 + (i % 5)}s`,
           }}
         />
       ))}
 
-      {Array.from({ length: 120 }).map((_, i) => (
+      {/* Embers */}
+      {Array.from({ length: 50 }).map((_, i) => (
         <span
           key={`ember-${i}`}
           className="corrupted-ember"
           style={{
             left: `${(i * 13) % 100}%`,
-            animationDelay: `${i * 0.05}s`,
-            animationDuration: `${6 + (i % 4)}s`,
+            animationDelay: `${i * 0.08}s`,
+            animationDuration: `${8 + (i % 3)}s`,
           }}
         />
       ))}
